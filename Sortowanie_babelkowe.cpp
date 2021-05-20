b2bb24fbe381d0881f4d0c2ea0e5bda50f9c039f
Adrian <56790744+zoladrian@users.noreply.github.com>
2021-05-21 00:19:45 +0200

    Add files via upload

---
+++ Sortowanie_babelkowe.cpp
@@ -0,0 +1,32 @@
+﻿//Sortowanie bąbelkowe
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+
+    int A[10] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+    int sortowanie(int& A);
+    {
+
+
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                if (A[j] > A[j + 1])
+                {
+                    swap(A[j], A[j + 1]);
+                }
+            }
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            cout << A[i] << " ";
+        }
+
+    }
+}
